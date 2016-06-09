1820c6389aa4521818ca55d0e612029cedf773ac
岡　隆史 <takashi.oka@pokelabo.co.jp>
2016-06-09 17:26:07 +0900

    Created by Coding Ground Client

---
+++ New Project-20160609/PlayerStatus.cs
@@ -0,0 +1,11 @@
+using System.IO;
+using System;
+
+class PlayerConst
+{
+    public int hp = PlayerConst.MAXHP;
+    public int mp = PlayerConst.MAXMP;
+    
+    public int def = PlayerConst.DEFFENCE_DEFAULT;
+    public int add_def = PlayerConst.
+}
