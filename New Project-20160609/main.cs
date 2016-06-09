8e118502209fb6d4b64f0b95a5d8b809b2a3340b
岡　隆史 <takashi.oka@pokelabo.co.jp>
2016-06-09 17:39:23 +0900

    Created by Coding Ground Client

---
+++ New Project-20160609/main.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Test test = new Test();
+        test.TestConsole();
+        Console.WriteLine("Hello, World!");
+    }
+}
