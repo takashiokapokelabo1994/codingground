d8fbdb24c39f1a6d47c66c6a1bcc656af22f85f7
岡　隆史 <takashi.oka@pokelabo.co.jp>
2016-06-10 11:37:05 +0900

    Created by Coding Ground Client

---
+++ New Project-20160609/PlayerConst.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using System;
+
+class PlayerConst{
+    public const int MAX_PHASE = 6;
+    public const int PARTY_MEMBER = 3;
+    public const int TURN_ON_OFF = 2;
+    
+    public const int MAXHP = 100;
+    public const int MAXMP = 30;
+    
+    public const int ATTACK = 10;
+    public const int DEF = 0;
+    
+    public const int ADD_ATTACK = 0;
+    public const int ADD_DEF    = 0;
+    
+    public const float LUCK     = 0;
+    public const float ADD_LUCK = 0;
+    
+    public const int ATTACK_PHASE = 0;
+    public const int MAGIC_PHASE  = 0;
+    public const int DEFFENCE_PHASE = 0;
+}
